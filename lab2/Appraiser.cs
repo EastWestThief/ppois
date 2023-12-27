b8f64fbb9ae7cc857a58e93f3e4b907779e963e2
EastWestThief <115877767+EastWestThief@users.noreply.github.com>
2023-12-28 02:10:57 +0300

    Add files via upload

---
+++ lab2/Appraiser.cs
@@ -0,0 +1,39 @@
+﻿public class Appraiser : Human
+{
+    public int ExperienceYears { get; set; } // Опыт работы в годах
+
+    // Метод для оценки картины
+    public double AppraisePainting(Picture painting)
+    {
+        // Например, мы можем оценивать картину на основе года создания и опыта оценщика
+        double Value = painting.Year.Year * ExperienceYears;
+
+        return Value;
+    }
+
+    public bool IsOriginal(Picture painting, List<Picture> allPaintings)
+    {
+        // Ищем картины с таким же названием
+        var sameTitlePaintings = allPaintings.Where(p => p.Title == painting.Title).ToList();
+
+        // Если нет других картин с таким же названием, то картина оригинальна
+        if (sameTitlePaintings.Count == 1)
+        {
+            return true;
+        }
+
+        // Если есть другие картины с таким же названием, сравниваем год создания
+        foreach (var sameTitlePainting in sameTitlePaintings)
+        {
+            if (sameTitlePainting.Year.CompareTo(painting.Year) < 0)
+            {
+                // Если найдена картина с таким же названием и более ранним годом создания, то картина не оригинальна
+                return false;
+            }
+        }
+
+        // Если не найдено других картин с таким же названием и более ранним годом создания, то картина оригинальна
+        return true;
+    }
+}
+
---
+++ lab2/Visitor.cs
@@ -0,0 +1,68 @@
+﻿public class Visitor : Human
+{
+    public List<Exhibition> Shows { get; set; }
+
+    public Visitor()
+    {
+        Shows = new List<Exhibition>();
+    }
+
+    public void LeaveComment(Exhibition exhibition, string text, Data date)
+    {
+        if (Shows.Contains(exhibition))
+        {
+            Comment comment = new Comment
+            {
+                Date = date,
+                Text = text,
+                Author = this,
+            };
+
+            exhibition.Comments.Add(comment);
+        }
+        else
+        {
+            throw new Exception("Посетитель не может оставить комментарий о выставке, которую он не посещал.");
+        }
+    }
+
+    public void VisitExhibition(Exhibition Name)
+    {
+        if (!Shows.Contains(Name))
+        {
+            Shows.Add(Name);
+        }
+        else
+        {
+            throw new Exception("Посетитель уже посещал эту выставку.");
+        }
+    }
+
+    public string FindPicture(Picture picture)
+    {
+        foreach (var exhibition in Shows)
+        {
+            if (exhibition.Paintings.Contains(picture))
+            {
+                return $"Картина '{picture.Title}' находится в галерее '{exhibition.CurrentGallery.Name}' на выставке '{exhibition.Name}'.";
+            }
+        }
+
+        foreach (var room in picture.CurrentGallery.Rooms)
+        {
+            if (room.Paintings.Contains(picture))
+            {
+                return $"Картина '{picture.Title}' находится в галерее '{picture.CurrentGallery.Name}' в комнате '{room.Name}'.";
+            }
+        }
+
+        if (picture.CurrentGallery.Storage.Contains(picture))
+        {
+            throw new Exception("Картина находится в хранилище и не доступна для просмотра.");
+        }
+
+        throw new Exception("Картина не найдена.");
+    }
+
+}
+
