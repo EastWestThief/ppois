a592b812a72864867746ea7a00ad7c95b5215b17
EastWestThief <115877767+EastWestThief@users.noreply.github.com>
2023-12-28 02:08:18 +0300

    Delete lab2 directory

--- lab2/Comment.cs
+++
@@ -1,7 +0,0 @@
-﻿public class Comment
-{
-    public Data Date { get; set; }
-    public string Text { get; set; }
-    public Visitor Author { get; set; }
-    
-}
--- lab2/Human.cs
+++
@@ -1,6 +0,0 @@
-﻿public class Human
-{
-    public string Name { get; set; }
-    public int Age { get; set; }
-
-}
